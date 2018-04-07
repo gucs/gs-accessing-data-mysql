93c599d7b521cb6c618f755bd5021915d9ca13ef
Jonathan <gucs.net@gmail.com>
2018-04-07 12:17:26 +0800

    1. ref lombok 2. GetMapping => PostMapping

--- complete/src/main/java/hello/User.java
+++ complete/src/main/java/hello/User.java
@@ -1,10 +1,19 @@
 package hello;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity // This tells Hibernate to make a table out of this class
 public class User {
     @Id
@@ -15,30 +24,5 @@ public class User {
 
     private String email;
 
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-    
-    
 }
 
